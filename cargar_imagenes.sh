8be626f73752fb0b4bb40def95ee749577a66919
Abel Gonzalez Gomez <abel.gonzalezgom@experis.es>
2023-02-07 09:29:01 +0100

    reducción de codigo

--- cargar_imagenes.sh
+++ cargar_imagenes.sh
@@ -2,10 +2,4 @@
 
 #En la máquina donde irán los contenedores, cargamos las imagenes
 
-podman load -i eurekaserver.tar
-podman load -i cnmweb.tar
-podman load -i clientescloud.tar
-podman load -i cloudnodemanagement.tar
-podman load -i cloudauditlogmanagement.tar
-podman load -i cloudusermanagement.tar
-podman load -i springgateway.tar
+podman load -i contenedor.tar
